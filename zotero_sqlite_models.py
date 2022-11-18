53a65bcbcedb1217f196d3fd0a23414d5687e3d3
fzyzcjy <ch271828n@outlook.com>
2022-11-18 14:54:31 +0800

    orm

--- zotero_sqlite_models.py
+++ zotero_sqlite_models.py
@@ -10,3 +10,44 @@ class Item(Base):
     itemID = Column(Integer, primary_key=True)
     dateAdded = Column(DateTime)
     key = Column(String)
+
+
+class ItemData(Base):
+    __tablename__ = "itemData"
+
+    itemID = Column(Integer, primary_key=True)
+    fieldID = Column(Integer)
+    valueID = Column(Integer)
+
+
+class ItemDataValue(Base):
+    __tablename__ = "itemDataValues"
+
+    valueID = Column(Integer, primary_key=True)
+    value = Column(String)
+
+
+class Field(Base):
+    __tablename__ = "fields"
+
+    fieldID = Column(Integer, primary_key=True)
+    fieldName = Column(String)
+    fieldFormatID = Column(Integer)
+
+
+class Creator(Base):
+    __tablename__ = "creators"
+
+    creatorID = Column(Integer, primary_key=True)
+    firstName = Column(String)
+    lastName = Column(String)
+    fieldMode = Column(Integer)
+
+
+class ItemCreator(Base):
+    __tablename__ = "itemCreators"
+
+    itemID = Column(Integer, primary_key=True)
+    creatorID = Column(Integer)
+    creatorTypeID = Column(Integer)
+    orderIndex = Column(Integer)
