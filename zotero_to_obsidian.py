13a891b9d3d4a2251d66b556a64dc186fe68fd8a
fzyzcjy <ch271828n@outlook.com>
2022-11-18 14:06:26 +0800

    scaffold

--- zotero_to_obsidian.py
+++ zotero_to_obsidian.py
@@ -1,1 +1,9 @@
-# TODO
+def main():
+    # ref
+    # https://www.zotero.org/support/dev/client_coding/direct_sqlite_database_access
+    # https://github.com/zotero/zotero-schema/tree/master
+    pass
+
+
+if __name__ == '__main__':
+    main()
